5b1745dc1a251562eed50af16ac9ec2277d45fd1
Tamerbekir <astoryofame@gmail.com>
2024-05-05 13:21:41 -0400

    Added error message to display if the user goes into the contact inputs but does not enter any information within them. Both name and email will display errors if left empty, and clear the error message once there is an input in the field

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,30 +1,51 @@
 
+//Displaying alert if user tries to submit information at this time (lack of functioning form)
 const submitForm = (e) => {
     e.preventDefault();
     alert('This is a work in progress! Please try again later!')
 }
 
+// Handleblur will check if the input is empty, and if it is a class will be added as an error, which will then display the message within the input field
+//however, if there is something within the input field the input will remove the error class and display nothing
+const handleBlur = (e) => {
+    if (!e.target.value) { 
+        e.target.classList.add('error'); 
+        e.target.nextElementSibling.style.display = 'block'; 
+    } else {
+        e.target.classList.remove('error')
+        e.target.nextElementSibling.style.display = 'none'; 
+    }
+};
 
+
+// If user does not enter information and hovers away from the input fields they will get a message within the input field
 export default function Contact() {
     return (
         <div className="text-center contact">
             <h1>Contact Me</h1>
             <p>This is the Contact me section.</p>
-            {/* added onClick function for form */}
-            <form>
+            {/* submit form will display an alert when clicked */}
+            <form noValidate onSubmit={ submitForm }>
                 <div className="mb-3">
                     <label className="form-label">Name</label>
-                    <input type="email" className="form-control" id="contactInputName" />
-                    <div id="contactMessage" className="form-text">We will never share your information with anyone else.</div>
+                    {/* on blur takes effect when user leaves the email section with any input  */}
+                    <input type="text" className="form-control" id="contactInputName" required onBlur={ handleBlur } />
+                    {/* display a style of nothing if user does not yet go into the input field to type someone ein */}
+                    <div className="errorName" style={{ display: 'none' }}>
+                        Please enter a name.
+                    </div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Email</label>
-                    {/* made changes to bootstrap boiler plate elements such as adjusting Form to div and closing input tag */}
-                    <input className="form-control" id="contactInputEmail" />
+                    {/* on blur takes effect when user leaves the email section with any input  */}
+                    <input type="email" className="form-control" id="contactInputEmail" required onBlur={ handleBlur } />
+                    {/* display a style of nothing if user does not yet go into the input field to type someone ein */}
+                    <div className="errorEmail" style={{ display: 'none' }}>
+                    Please enter an email.
+                    </div>
                 </div>
-                <button onClick={ submitForm } type="submit" className="submitContact btn btn-primary m-3">Submit</button>
+                <button type="submit" className="btn btn-primary m-3">Submit</button>
             </form>
         </div>
     );
 }
-
