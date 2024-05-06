2e162b4cacb7994de9b085f97b70035ebf73c771
Tamerbekir <astoryofame@gmail.com>
2024-05-05 20:59:55 -0400

    Created footer links for socials and stack overflow

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,6 +4,14 @@
 export default function Footer() {
     return (
         <footer className="text-center footer">
+            <ul className="text-center foot-ul">
+                <li className="text- center footer-list">
+                    <a href="http://www.github.come/tamerbekir">GitHub</a>
+                    <a href="https://stackoverflow.com/users/22620932/tam">Stack Overflow</a>
+                    <a href="https://www.linkedin.com/in/tam-b-53815035/">LinkedIn</a>
+                    <a href="https://www.instagram.com/keplertam/">Instagram</a>
+                </li>
+            </ul>
             <p>Created with 💙 by Tamer Bekir</p>
         </footer>
     );
